f374b4bd70185572d7d792824be9723a64f96b5f
Armel Steve <ouetchouaarmel@yahoo.com>
2021-04-13 12:01:55 +0200

    creation des solution

---
+++ Applicationcc1MVC/Controllers/HommeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Applicationcc1MVC.Controllers
+{
+    public class HommeController : Controller
+    {
+        // GET: Homme
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
